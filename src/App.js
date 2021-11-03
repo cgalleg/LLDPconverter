1c2952927a59d36354cd66a120dadd51738a6367
cgalleg <crisd.gallegos@gmail.com>
2021-11-03 18:26:40 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [color,setColor]= useState("Red");
+
+  function inputChange(e){
+    console.log(e.target.value);
+    setColor(e.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Welcome name={"Cristian"}/>
+        <h1>{color}</h1>
+        <input onChange={inputChange}></input>
       </header>
     </div>
   );
+}
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
 }
 
 export default App;
